d6c4e11629aed7c9e730a3734446344117616fb8
hernandez87v <hernandez87v@hotmail.com>
2020-07-11 10:28:22 -0700

    Width and Height adjust

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
 import Twitch from './components/TwitchPlayer/Twitch';
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Twitch></Twitch>
+      {/* <Footer></Footer> */}
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer id="footer">
+      {/* < !--Copyright -- > */}
+      <div class="footer-copyright text-center py-3">
+        © 2020 Copyright:
+        <a> DJ Jose Hernandez </a>
+      </div>
+      {/* <!--Copyright --> */}
+    </footer>
+    // < !--Footer -->
+  );
+}
+
+export default Footer;
