c07457a923822f24376569451d2a689ff92f09d5
hernandez87v <hernandez87v@hotmail.com>
2020-07-30 11:48:40 -0700

    Add Mixcloud player/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
-import Twitch from './components/TwitchPlayer/Twitch';
-// import Footer from './components/Footer/Footer';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import SoundCloud from './components/Soundcloud/Soundcloud';
 import Home from './components/Home/Home';
+import Twitch from './components/TwitchPlayer/Twitch';
+import SoundCloud from './components/Soundcloud/Soundcloud';
+import Mixcloud from './components/Mixcloud/Mixcloud';
+
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -25,6 +27,9 @@ function App() {
           </Route>
           <Route path="/Twitch" component={Twitch}>
             <Twitch />
+          </Route>
+          <Route path="/Mixcloud" component={Mixcloud}>
+            <Mixcloud />
           </Route>
         </Switch>
       </main>
---
+++ src/components/Mixcloud/Mixcloud.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactPlayer from 'react-player/mixcloud';
+import './Mixcloud.css';
+
+// ,Loads Mixcloud Player
+function Mixcloud() {
+  return (
+    <div className="mixcloud-player">
+      <ReactPlayer
+        height="200px"
+        width="100%"
+        url="https://www.mixcloud.com/vancouvertrancefamily/vtf-radio-66-jose-hernandez/"
+      />
+    </div>
+  );
+}
+
+export default Mixcloud;
