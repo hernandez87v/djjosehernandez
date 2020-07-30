f43ce3967d74b3feec6997b849cb44bf269e5608
hernandez87v <hernandez87v@hotmail.com>
2020-07-30 15:08:16 -0700

    All players appear within URL path but only with refresh

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
-import Home from './components/Home/Home';
+// import Home from './components/Home/Home';
 import Twitch from './components/TwitchPlayer/Twitch';
 import SoundCloud from './components/Soundcloud/Soundcloud';
 import Mixcloud from './components/Mixcloud/Mixcloud';
@@ -17,7 +17,7 @@ function App() {
         <Route path="/Twitch" exact component={Twitch} />
         <Route path="/Soundcloud" exact component={SoundCloud} />
         <Route path="/Mixcloud" exact component={Mixcloud} />
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Twitch} />
       </Switch>
     </div>
   );
