b4e72228f1745be2da68b538b3baafb794efca41
hernandez87v <hernandez87v@hotmail.com>
2020-07-30 15:32:37 -0700

    Still need to fix routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { BrowserRouter as Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
 // import Home from './components/Home/Home';
 import Twitch from './components/TwitchPlayer/Twitch';
-import SoundCloud from './components/Soundcloud/Soundcloud';
+import Soundcloud from './components/Soundcloud/Soundcloud';
 import Mixcloud from './components/Mixcloud/Mixcloud';
 
 // import Footer from './components/Footer/Footer';
@@ -14,12 +14,12 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Switch>
+      <Router>
         <Route path="/Twitch" exact component={Twitch} />
-        <Route path="/Soundcloud" exact component={SoundCloud} />
+        <Route path="/Soundcloud" exact component={Soundcloud} />
         <Route path="/Mixcloud" exact component={Mixcloud} />
         <Route path="/" exact component={Twitch} />
-      </Switch>
+      </Router>
     </div>
   );
 }
