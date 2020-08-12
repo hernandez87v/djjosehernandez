c43e2b36ffd24c913e19026be93baaa02417406d
hernandez87v <hernandez87v@hotmail.com>
2020-08-12 11:57:28 -0700

    Restored Navbar links to work with react router

--- src/components/NavBar/Navbar2.jsx
+++ src/components/NavBar/Navbar2.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Navbar2.css';
 import { Link } from 'react-router-dom';
-import ReactGA from 'react-ga';
 
 export default function Soundcloud() {
   return (
@@ -12,40 +11,19 @@ export default function Soundcloud() {
       <nav>
         <ul className="nav__links">
           <li>
-            <Link to="/Twitch">
-              <ReactGA.OutboundLink eventLabel="Twitch_toPage" to="/Twitch">
-                Twitch
-              </ReactGA.OutboundLink>
-            </Link>
+            <Link to="/Twitch">Twitch</Link>
           </li>
           <li>
-            <Link>
-              <ReactGA.OutboundLink
-                eventLabel="Soundcloud_toPage"
-                to="/Soundcloud"
-              >
-                Soundcloud
-              </ReactGA.OutboundLink>
-            </Link>
+            <Link to="/Soundcloud">Soundcloud</Link>
           </li>
           <li>
-            <Link>
-              <ReactGA.OutboundLink eventLabel="Mixcloud_toPage" to="/Mixcloud">
-                Mixcloud
-              </ReactGA.OutboundLink>
-            </Link>
+            <Link to="/Mixcloud">Mixcloud</Link>
           </li>
         </ul>
       </nav>
-      <Link>
-        <ReactGA.OutboundLink
-          className="cta"
-          eventLabel="Email_Contact"
-          to="mailto:djjosehernandez@hotmail.com"
-        >
-          <button>Contact</button>
-        </ReactGA.OutboundLink>
-      </Link>
+      <a className="cta" href="mailto:djjosehernandez@hotmail.com">
+        <button>Contact</button>
+      </a>
     </header>
   );
 }
