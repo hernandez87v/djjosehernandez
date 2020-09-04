23821f78542b91f79f36cf3c46b36299b18eaad8
hernandez87v <hernandez87v@hotmail.com>
2020-09-04 14:02:33 -0700

    Remove index.css as not used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
