cbbd9e03e0cc1dd48e7d8471e05ba1a69c5a253b
hernandez87v <hernandez87v@hotmail.com>
2020-09-10 15:19:47 -0700

    fix- footer bottom gap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter forceRefresh={true}>
@@ -11,5 +9,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-// serviceWorker.register();
