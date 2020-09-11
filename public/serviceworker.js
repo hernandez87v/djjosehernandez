2cb35564f0bffb25cf050f102a100eeb371684bf
hernandez87v <hernandez87v@hotmail.com>
2020-09-11 15:25:04 -0700

    delete old serviceworker file

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,40 +1,41 @@
-const CACHE_NAME = 'version-1';
-const urlsToCache = ['index.html', 'offline.html'];
-
+const cacheName = 'version-1';
 const self = this;
-
 // Install SW
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  console.log('Service Worker: Installed');
 });
-
-// Listen for requests
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match('offline.html'));
-    })
-  );
-});
-
 // Activate the SW
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE_NAME);
-
+  console.log('Service Worker: Activated');
+  // Remove unwanted caches
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          console.log('Service Worker: Clearing Old Cache');
+          if (cache !== cacheName) {
+            return caches.delete(cache);
           }
         })
-      )
-    )
+      );
+    })
+  );
+});
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+  console.log('Service Worker: Fetching...');
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        // Make clone of response
+        const resClone = res.clone();
+        // Open Cache
+        caches.open(cacheName).then((cache) => {
+          // Add response to cache
+          cache.put(event.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => caches.match(event.request).then((res) => res))
   );
 });
