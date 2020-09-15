8cc537eebe6fbc38dcb029e1c9dc123eea2bcbb9
hernandez87v <hernandez87v@hotmail.com>
2020-09-15 13:53:32 -0700

    mixcloud api now working

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -24,6 +24,7 @@ function Home() {
           height="60"
           src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&autoplay=1&feed=%2FDjJoseHernandez%2F"
           frameBorder="0"
+          allow="autoplay"
         ></iframe>
       </div>
       <section rel="preload" style={sectionStyle}>
@@ -34,3 +35,7 @@ function Home() {
 }
 
 export default Home;
+
+{
+  /* <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&autoplay=1&feed=%2FDjJoseHernandez%2Fmakingprorgess-037%2F" frameborder="0" allow="autoplay"></iframe> */
+}
