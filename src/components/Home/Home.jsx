d4ac497e4137ef6c9614db16a6dc1a3f32e16b15
hernandez87v <hernandez87v@hotmail.com>
2020-09-15 13:57:58 -0700

    restored yarn files

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -35,7 +35,3 @@ function Home() {
 }
 
 export default Home;
-
-{
-  /* <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&autoplay=1&feed=%2FDjJoseHernandez%2Fmakingprorgess-037%2F" frameborder="0" allow="autoplay"></iframe> */
-}
