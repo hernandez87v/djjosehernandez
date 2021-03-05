2aa47a975aa9b62c1a1bd993b84abf835975eb65
hernandez87v <hernandez87v@hotmail.com>
2021-03-05 15:15:47 -0800

    got IG api working and stylized- working on footer

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import './Gallery.css';
+import './Gallery.css';
 
 function Gallery() {
   const [post, setPosts] = useState([]);
@@ -8,20 +8,44 @@ function Gallery() {
   useEffect(() => {
     axios
       .get(
-        `https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp&access_token=${process.env.REACT_APP_API_TOKEN}`
+        `https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp,caption&access_token=${process.env.REACT_APP_IG_TOKEN}`
       )
-      .then((response) => setPosts(response.data.data));
+      .then((response) => setPosts(response.data.data))
+      .catch(function (error) {
+        console.log('API Error: ', error.message);
+      });
   }, []);
-  console.log('this is posts:', post);
+  // console.log('this is posts:', post);
 
   return (
-    <div className="gallery">
+    <div className="all-cards">
       {post.map((posts) => (
         <div className="card-content" key={posts.id}>
-          <ul>
-            <li>id: {posts.id}</li>
-          </ul>
-          <img className="feed" src={posts.media_url} />
+          <div className="post-container">
+            {posts.media_type === 'IMAGE' ||
+            posts.media_type === 'CAROUSEL_ALBUM' ? (
+              <img className="single-post" src={posts.media_url} />
+            ) : (
+              <video
+                controls
+                autoPlay
+                loop
+                muted
+                className="video-post"
+                width="127.75"
+                height="127.75"
+              >
+                <source src={posts.media_url} type="video/mp4" />
+              </video>
+            )}
+          </div>
+          <div className="post-info">
+            <span className="user-name">{posts.username}</span>
+            <span className="post-timestamp">{posts.timestamp}</span>
+          </div>
+          {/* <div className="caption-container">
+            <span className="user-caption">{posts.caption}</span>
+          </div> */}
         </div>
       ))}
     </div>
